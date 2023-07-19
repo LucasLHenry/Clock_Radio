9581fe7e19c2757d88476c42d9dff5cbcb2df34e
Lucas Henry <lucasleroyhenry@gmail.com>
2023-07-19 14:29:30 -0700

    changed clock_class

--- button_class.py
+++ button_class.py
@@ -10,6 +10,17 @@ class Button:
         if self.pin.value() == 0:
             self.held_db = True
 
+    # the debounce logic is as follows:
+    #   on interrupt, set internal value to correct value as read from pin
+    #   shut off interrupts
+    #   set a timer for BUTTON_DOWNTIME_MS milliseconds, and call _timer_end once it's over
+    #   _timer_end reattaches interrupts.
+    # This means that only one interrupt can be called within a certain period
+
+    # Additionally, there is logic for detecting a held button as well. When the button is 
+    # pressed down, set a timer for BUTTON_HOLDTIME_MS. If the button is released before it 
+    # goes off, the timer is shut off. If it isn't released, the value is checked and as 
+    # logn as it's still high the hold doorbell is set. 
     def _handler(self, pin):
         val = self.pin.value()
         self.pin.irq(handler=None)
@@ -28,6 +39,8 @@ class Button:
         self.held_db = False
         self.pin.irq(handler=self._handler, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
 
+    # these values are doorbells, so will get reset once they're read
+    # means only one reading is available at a time
     def get(self):
         to_send = (self.db, self.held_db)
         (self.db, self.held_db) = (False, False)
--- clock_class.py
+++ clock_class.py
@@ -1,20 +1,14 @@
 from machine import RTC
 from settings_lib import CLOCK_START_DATETIME
 
+# simple wrapper for the machine.RTC class, allows parsing and the ability to set an alarm
+# however, this is not a real alarm, it just stores a time. So it must be checked externally
+# whether or not the alarm has gone off
 class Clock():
     def __init__(self):
         self.rtc = RTC()
         self.rtc.datetime(CLOCK_START_DATETIME)
-        self.alarm = (0, 0) # (minute, second)
-        self.alarm_on = False
-
-    def parse_datetime(self):
-        (_, _, _, _, hour, minute, second, _) = self.rtc.datetime()
-        return (hour, minute, second)
-    
-    def get_time_str(self):
-        (hour, minute, _) = self.parse_datetime()
-        return "{:d}:{:02d}".format(hour, minute)
+        self.alarm = (0, 0) # (hour, minute)
     
     def set_alarm(self, alarm_hour, alarm_minute):
         if not (isinstance(alarm_hour, int) or isinstance(alarm_minute, int)):
@@ -22,3 +16,24 @@ class Clock():
         if alarm_hour < 0 or alarm_hour > 23 or alarm_minute < 0 or alarm_minute > 59:
             return False
         self.alarm = (alarm_hour, alarm_minute)
+    
+    def get_alarm(self):
+        return self.alarm
+    
+    def set_time(self, hour, minute):
+        if not (isinstance(hour, int) or isinstance(minute, int)):
+            return False
+        if hour < 0 or hour > 23 or minute < 0 or minute > 59:
+            return False
+        (year, month, day, _, _, second, microsecond, tzinfo) = self.rtc.datetime()
+        self.rtc.datetime((year, month, day, hour, minute, second, microsecond, tzinfo))
+
+    def get_time(self):
+        (_, _, _, _, hour, minute, _, _) = self.rtc.datetime()
+        return (hour, minute)
+    
+    def alarm_pending(self):
+        (hour, minute) = self.get_time()
+        if hour > self.alarm[0] or (hour == self.alarm[0] and minute >= self.alarm[1]):
+            return True
+        return False
